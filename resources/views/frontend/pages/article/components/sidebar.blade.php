c66f2866a7e3ff05e2d48706abd8f903fe3d31a5
Sandip Dangal <sandipdangal77@gmail.com>
2023-03-26 12:12:19 +0545

    fixed you may also like error

--- resources/views/frontend/pages/article/components/sidebar.blade.php
+++ resources/views/frontend/pages/article/components/sidebar.blade.php
@@ -4,24 +4,26 @@
     </div>
 </div>
 <div class="row">
-    @foreach ($sidebar_articles as $article )
-        <div class="sidebar-post col-md-6 col-lg-12 mt-3">
-            <div class="image">
-                <figure class="m-0">
-                    <a href="{{ route('singleArticle', $article->slug) }}/">
-                        <img src="{{ asset('/uploads/thumbnail/'.$article->image) }}" alt="{{ $article->title }}" class="image_img">
-                    </a>
-                </figure>
-            </div>
-            <div class="sidebar-post-title mx-2">
+    @isset ($sidebar_articles)
+        @foreach ($sidebar_articles as $article )
+            <div class="sidebar-post col-md-6 col-lg-12 mt-3">
+                <div class="image">
+                    <figure class="m-0">
+                        <a href="{{ route('singleArticle', $article->slug) }}/">
+                            <img src="{{ asset('/uploads/thumbnail/'.$article->image) }}" alt="{{ $article->title }}" class="image_img">
+                        </a>
+                    </figure>
+                </div>
+                <div class="sidebar-post-title mx-2">
 
-                <h2 class="title">
-                    <a href="{{ route('singleArticle', $article->slug) }}/">
-                        {{ $article->title  }}
-                    </a>
-                </h2>
-            </div>
+                    <h2 class="title">
+                        <a href="{{ route('singleArticle', $article->slug) }}/">
+                            {{ $article->title  }}
+                        </a>
+                    </h2>
+                </div>
 
-        </div>
-    @endforeach
+            </div>
+        @endforeach
+    @endisset
 </div>
